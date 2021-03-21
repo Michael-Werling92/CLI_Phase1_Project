1c6a5d83f5f330d01ecde15e36e2b2e370af8782
Michael <mfw6mw@virginia.edu>
2021-03-21 13:06:44 -0400

    added to API

---
+++ lib/cli.rb
@@ -0,0 +1,12 @@
+class CLI
+
+    def run
+        puts "Welcome to the Pokemon Type Battle Stats Tracker"
+        continue
+        API.load_type
+    end
+
+    def continue
+        print "Press any key to continue\r"
+        gets
+    end
